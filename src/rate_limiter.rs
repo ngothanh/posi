b5cde2c370538a9f8c9670341ef98f5777c46611
edward.ngo <thanh.ngo.2510@gmail.com>
2024-06-13 21:46:42 +0700

    Finished implementing FixedWindowRateLimiter

--- src/rate_limiter.rs
+++ src/rate_limiter.rs
@@ -1,5 +1,5 @@
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use crate::scheduler::Scheduler;
 
@@ -11,6 +11,52 @@ struct TokenBucketRateLimiter {
     rate: Rate,
     permits: Arc<Mutex<usize>>,
     schedulers: Scheduler,
+}
+
+struct FixedWindowRateLimiter {
+    rate: Rate,
+    counter: Arc<Mutex<usize>>,
+    window_start: Arc<Mutex<Instant>>,
+}
+
+
+impl FixedWindowRateLimiter {
+    pub fn new(rate: Rate) -> FixedWindowRateLimiter {
+        let permit_num = rate.permit_num;
+        FixedWindowRateLimiter {
+            rate,
+            counter: Arc::new(Mutex::new(0)),
+            window_start: Arc::new(Mutex::new(Instant::now())),
+        }
+    }
+
+    pub fn reset_window(&self) {
+        let mut counter = self.counter.lock().unwrap();
+        let mut window_start = self.window_start.lock().unwrap();
+        *counter = 0;
+        *window_start = Instant::now()
+    }
+}
+
+impl RateLimiter for FixedWindowRateLimiter {
+    fn try_acquire(&self, permits: usize) -> bool {
+        let now = Instant::now();
+        let window_start = self.window_start.lock().unwrap();
+        let duration = self.rate.duration;
+        if now.duration_since(*window_start) > duration {
+            drop(window_start);
+            self.reset_window();
+        }
+
+        let permit_num = self.rate.permit_num;
+        let mut cur_counter = self.counter.lock().unwrap();
+        if *cur_counter + permits > permit_num {
+            return false;
+        }
+
+        *cur_counter += permits;
+        true
+    }
 }
 
 impl TokenBucketRateLimiter {
@@ -66,7 +112,7 @@ mod tests {
     use std::thread;
     use std::time::Duration;
 
-    use crate::rate_limiter::{Rate, RateLimiter, TokenBucketRateLimiter};
+    use crate::rate_limiter::{FixedWindowRateLimiter, Rate, RateLimiter, TokenBucketRateLimiter};
 
     #[test]
     fn give_token_bucket_rate_limiter_then_it_protects_the_resource_correctly() {
@@ -87,5 +133,22 @@ mod tests {
         assert_eq!(rate_limiter.try_acquire(3), true);
 
         return;
+    }
+
+    #[test]
+    fn given_fixed_window_rate_limiter_then_it_protects_the_resource_correctly() {
+        //given
+        let rate = Rate {
+            permit_num: 5,
+            duration: Duration::from_secs(3),
+        };
+        let rate_limiter = FixedWindowRateLimiter::new(rate);
+
+        //then
+        assert_eq!(rate_limiter.try_acquire(5), true);
+        assert_eq!(rate_limiter.try_acquire(1), false);
+
+        thread::sleep(Duration::from_secs(5));
+        assert_eq!(rate_limiter.try_acquire(5), true);
     }
 }
